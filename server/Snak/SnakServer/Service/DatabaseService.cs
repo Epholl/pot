f6f31e04140d44d34ae3dc107b9020402502fc5f
Tomas Istenik <tomas.istenik@inloop.eu>
2016-01-08 15:52:41 +0100

    Basic SQLite DB added, DB layer in preparation..

---
+++ server/Snak/SnakServer/Service/DatabaseService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+using System.Data.SQLite;
+using System.Data.Entity;
+using System.Reflection;
+using System.Data.Entity.Core.Common;
+using System.Data.SQLite.EF6;
+
+namespace SnakServer.Service
+{
+    class DatabaseService
+    {
+    }
+
+    public class SQLiteConnectionFactory : IDbConnectionFactory
+    {
+        public DbConnection CreateConnection(string nameOrConnectionString)
+        {
+            return new SQLiteConnection(nameOrConnectionString);
+        }
+    }
+
+    public class SQLiteConfiguration : DbConfiguration
+    {
+        public SQLiteConfiguration()
+        {
+            SetDefaultConnectionFactory(new SQLiteConnectionFactory());
+            SetProviderFactory("System.Data.SQLite", SQLiteFactory.Instance);
+            SetProviderFactory("System.Data.SQLite.EF6", SQLiteProviderFactory.Instance);
+            Type t = Type.GetType("System.Data.SQLite.EF6.SQLiteProviderServices, System.Data.SQLite.EF6");
+            FieldInfo fi = t.GetField("Instance", BindingFlags.NonPublic | BindingFlags.Static);
+            SetProviderServices("System.Data.SQLite", (DbProviderServices)fi.GetValue(null));
+        }
+    }
+}
