67b33b6d0dcfbc1fde1cf7c62de13849b18b26fb
Tomas Istenik <tomas.istenik@inloop.eu>
2016-01-09 22:25:24 +0100

    Added unity client, needs comments

---
+++ client/Snak/Assets/Scripts/WCFTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WCFTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
